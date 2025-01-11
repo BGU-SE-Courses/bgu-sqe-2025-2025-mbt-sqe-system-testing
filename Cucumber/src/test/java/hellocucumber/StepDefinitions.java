d71bdd5113d11989fc3796a6b30ccad0defa1673
DavidVolodarsky1 <david.volodarski1@gmail.com>
2025-01-12 00:01:30 +0200

    exmaple feature, in hellocucumber i started new class Addcomment, and statred StepDefinitions

--- Cucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ Cucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -4,21 +4,68 @@ import io.cucumber.java.en.*;
 
 import org.junit.jupiter.api.Assertions.*;
 
+import io.cucumber.java.en.*;
+
 public class StepDefinitions {
 
-    // $$*TODO* explain what this step does$$
-    @Given("an example scenario")
-    public void anExampleScenario() {
+    AddComment actuator = new AddComment();
+
+    @Given("A product {string} exists in the store")
+    public void productExistsInStore(String productName) {
+        actuator.Init();
+        actuator.VerifyProductExists(productName);
     }
 
-    // $$*TODO* explain what this step does$$
-    @When("all step definitions are implemented")
-    public void allStepDefinitionsAreImplemented() {
+    @Given("The user {string} is logged in with the password {string}")
+    public void userLogsIn(String username, String password) {
+        actuator.NavigateToLogin();
+        actuator.Login(username, password);
+        actuator.VerifyLogin();
     }
 
-    // $$*TODO* explain what this step does$$
-    @Then("the scenario passes")
-    public void theScenarioPasses() {
+    @When("The user navigates to the product {string} page")
+    public void userNavigatesToProductPage(String productName) {
+        actuator.NavigateToProduct(productName);
     }
 
+    @When("The user writes a comment {string}")
+    public void userWritesComment(String comment) {
+        actuator.WriteComment(comment);
+    }
+
+    @Then("The comment {string} is visible under the product {string}")
+    public void verifyCommentIsVisible(String comment, String productName) {
+        actuator.VerifyCommentVisible(productName, comment);
+    }
+
+    @Given("A product {string} exists in the store with a comment {string}")
+    public void productExistsWithComment(String productName, String comment) {
+        actuator.VerifyProductExists(productName);
+        actuator.VerifyCommentVisible(productName, comment);
+    }
+
+    @Given("The admin is logged in with username {string} and password {string}")
+    public void adminLogsIn(String username, String password) {
+        actuator.NavigateToAdminLogin();
+        actuator.AdminLogin(username, password);
+    }
+
+    @When("The admin deletes the product {string}")
+    public void adminDeletesProduct(String productName) {
+        actuator.DeleteProduct(productName);
+    }
+
+    @When("The user logs in with username {string} and password {string}")
+    public void userLogsInAgain(String username, String password) {
+        actuator.NavigateToLogin();
+        actuator.Login(username, password);
+        actuator.VerifyLogin();
+    }
+
+    @Then("The product {string} and the comment {string} are no longer visible")
+    public void verifyProductAndCommentNotVisible(String productName, String comment) {
+        actuator.VerifyProductDeleted(productName);
+        actuator.VerifyCommentNotVisible(productName, comment);
+        actuator.Close();
+    }
 }
--- Cucumber/src/test/resources/hellocucumber/example.feature
+++ Cucumber/src/test/resources/hellocucumber/example.feature
@@ -1,6 +1,25 @@
-Feature: A set of scenarios for testing the "example" module
+Feature: User writes a comment on a product and the admin deletes the product
 
-  Scenario: Testing how a case where a user adds a product to the cart
-    Given an example scenario
-    When all step definitions are implemented
-    Then the scenario passes
+  Scenario Outline: A user writes a comment on a product
+    Given A product "<product_name>" exists in the store
+    And The user "<user_username>" is logged in with the password "<user_password>"
+    When The user navigates to the product "<product_name>" page
+    And The user writes a comment "<comment_text>"
+    Then The comment "<comment_text>" is visible under the product "<product_name>"
+
+    Examples:
+      | product_name         | user_username      | user_password | comment_text                  |
+      | Hummingbird T-shirt  | customer@example.com | customer123  | "I love this product!"       |
+      | Printed Mug          | testuser@example.com | testpass     | "Highly recommend this mug!" |
+
+  Scenario Outline: Admin deletes the product after the user commented
+    Given A product "<product_name>" exists in the store with a comment "<comment_text>"
+    And The admin is logged in with username "<admin_username>" and password "<admin_password>"
+    When The admin deletes the product "<product_name>"
+    And The user logs in with username "<user_username>" and password "<user_password>"
+    Then The product "<product_name>" and the comment "<comment_text>" are no longer visible
+
+    Examples:
+      | product_name         | user_username      | user_password | comment_text               | admin_username     | admin_password |
+      | Hummingbird T-shirt  | customer@example.com | customer123  | "I love this product!"    | admin@example.com  | admin123       |
+      | Printed Mug          | testuser@example.com | testpass     | "Highly recommend this mug!" | admin@example.com  | admin123       |
