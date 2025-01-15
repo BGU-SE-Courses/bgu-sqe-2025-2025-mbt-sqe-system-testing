48d01d04083a1fdd36a10aa57dadbd86c15cc722
assayagt <assayagt@post.bgu.ac.il>
2025-01-15 11:46:31 +0200

    Added cucumber tests

--- Cucumber/src/test/java/hellocucumber/DeleteProductActuatorAdmin.java
+++ Cucumber/src/test/java/hellocucumber/DeleteProductActuatorAdmin.java
@@ -1,13 +1,12 @@
 package hellocucumber;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Point;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class DeleteProductActuatorAdmin {
     private static WebDriver driver;
@@ -27,10 +26,11 @@ public class DeleteProductActuatorAdmin {
         driver.get("http://localhost/my_shop/admin4150un9qxx4ji25cqkv/");
 
         // Maximize window to ensure proper display
-        driver.manage().window().setPosition(new Point(0, 0));
+        driver.manage().window().maximize();
 
         System.out.println("Driver setup finished for - " + driver.getTitle());
     }
+
 
     public void enterLoginInfoAdmin(String username, String password) {
         // locate the username input box and enter username
@@ -38,7 +38,23 @@ public class DeleteProductActuatorAdmin {
         // locate the password input box and enter password
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name=\"passwd\"]"))).sendKeys(password);
         // locate Log in button and press
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='submit_login']/span[1]"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='submit_login']"))).click();
         wait.until(ExpectedConditions.titleContains("Home"));
+        // Wait for the dashboard to load
     }
+
+    public void deleteProduct() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//html/body/nav/div/ul/li[6]/a"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='subtab-AdminProducts']/a[1]"))).click();
+
+        // Locate the input field and clear it before entering text
+        WebElement productNameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='product_name']")));
+        productNameField.clear(); // Clear existing text
+        productNameField.sendKeys("T-Shirt" + Keys.ENTER); // Enter new text and trigger search
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[2]/div/div[3]/div[2]/div/form[2]/table/tbody/tr/td[11]/div/div/a[2]"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@data-title, \"Delete selection\")]"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(@class, 'btn-confirm-submit')]"))).click();
+    }
+
 }
