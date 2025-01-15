ac1b3fa7c3ffa30a0aa7f7e1c1baa286e6d070ee
Yoav Heller <yoavhell@post.bgu.ac.il>
2025-01-15 11:25:39 +0200

    gal and ofek did some provengo work

--- Provengo/helloprovengo/data/data.js
+++ Provengo/helloprovengo/data/data.js
@@ -4,12 +4,33 @@
 
 const URL = 'https://www.google.com/?hl=en';
 
+adminDetails = {
+  username: "admin",
+  password: "1234"
+}
 const xpaths = {
   searchWindow: {
     searchInput: '//textarea[@aria-label="Search"]',
     searchButton: '//input[@aria-label="Google Search"]',
     feelingLuckyButton: '//input[@aria-label="I\'m Feeling Lucky"]'
+  },
+  loginPageAdmin:{
+    usernameInput : "//*[@id='input-username']",
+    passwordInput : "//*[@id='input-password']",
+    submitButton : "//button[1]"
+  },
+  dashboardPageAdmin:{
+    marketingClick: "//nav[1]/ul[1]/li[7]/a[1]",
+    couponsButton: "//nav[1]/ul[1]/li[7]/ul[1]/li[3]/a[1]"
+  },
+  couponListPage:{
+    editClick : "//tr[1]/td[8]/a[1]"
+  },
+  couponPage:{
+    saveButtonClick : "//div[1]/div[1]/button[1]",
+    statusButtonClick: "//*[@id='input-status']"
   }
+
 }
 
 const searchTerm = 'pizza'
--- Provengo/helloprovengo/spec/js/actions.js
+++ Provengo/helloprovengo/spec/js/actions.js
@@ -1,15 +1,34 @@
-function composeQuery(session, data) {
-  session.writeText(xpaths.searchWindow.searchInput, data.text)
+// function composeQuery(session, data) {
+//   session.writeText(xpaths.searchWindow.searchInput, data.text)
+// }
+//
+// function startSearch(session) {
+//   with(session) {
+//     click(xpaths.searchWindow.searchButton)
+//   }
+// }
+//
+// function feelLucky(session) {
+//   with(session) {
+//     click(xpaths.searchWindow.feelingLuckyButton)
+//   }
+// }
+
+function loginAsAdmin(session, username, password) {
+  session.writeText(xpaths.loginPageAdmin.usernameInput, username);
+  session.writeText(xpaths.loginPageAdmin.passwordInput, password);
+  session.click(xpaths.loginPageAdmin.submitButton);
 }
 
-function startSearch(session) {
-  with(session) {
-    click(xpaths.searchWindow.searchButton)
-  }
+function navigateToCoupons(session) {
+  session.click(xpaths.dashboardPageAdmin.marketingClick);
+  session.click(xpaths.dashboardPageAdmin.couponsButton);
 }
 
-function feelLucky(session) {
-  with(session) {
-    click(xpaths.searchWindow.feelingLuckyButton)
-  }
+function disableCoupon(session) {
+  session.click(xpaths.couponListPage.editClick);
+  window.scrollTo(0, document.body.scrollHeight);
+  session.click(xpaths.couponPage.statusButtonClick);
+  window.scrollTo(0, 0);
+  session.click(xpaths.couponPage.saveButtonClick);
 }
