d997039387d28223338a95294c05b35c1504e220
Eden Nahum <edennahu@post.bgu.ac.il>
2025-01-18 23:42:59 +0200

    finish cucumber part

--- Cucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ Cucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -1,25 +1,90 @@
 package hellocucumber;
 
-import io.cucumber.java.en.*;
+import java.time.Duration;
 
-import org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 
 public class StepDefinitions {
 
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+
     // $$*TODO* explain what this step does$$
-    @Given("an example scenario")
-    public void anExampleScenario() {
+    @Given("Student is on Home page")
+    public void studentIsOnHomePage() {
+        driver = new SafariDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+        driver.get("http://localhost:8888/moodle405/");
+    }
+
+    @And("Student is logged in with {string} and {string}")
+    public void studentIsLoggedIn(String username, String password){
+        WebElement loginElement = driver.findElement(By.xpath("//*[@id=\"usernavigation\"]/div/div/span/a"));
+        loginElement.click();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[1]/form[1]/div[1]/input[1]"))).sendKeys(username);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[2]/div[1]/input[1]"))).sendKeys(password);
+        driver.findElement(By.xpath("//form[1]/div[3]/button[1]")).click();
+    }
+
+    @And("Studnt has a course")
+    public void studentHasACourse(){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        WebElement courseElement = driver.findElement(By.xpath("//div[1]/div[1]/h3[1]/a[1]"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", courseElement);
+        courseElement.click();
+    }
+
+    @And("Course has a forum discussion")
+    public void CourseHasAForum(){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.xpath("//div[1]/div[2]/div[2]/div[1]/div[1]/a[1]")).click();
     }
 
     // $$*TODO* explain what this step does$$
-    @When("all step definitions are implemented")
-    public void allStepDefinitionsAreImplemented() {
+    @When("Student subscribes to a forum discussion")
+    public void studentSubscribesToADiscussion() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.xpath("//td[5]/div[1]")).click();
     }
 
     // $$*TODO* explain what this step does$$
-    @Then("the scenario passes")
+    @Then("The student succcessfully subscribed to the discussion")
     public void theScenarioPasses() {
-        
+        driver.findElement(By.xpath("//td[5]/div[1]"));
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- Cucumber/src/test/resources/hellocucumber/example.feature
+++ Cucumber/src/test/resources/hellocucumber/example.feature
@@ -1,6 +1,21 @@
-Feature: A set of scenarios for testing the "example" module
+# Feature: A set of scenarios for testing the "example" module
 
-  Scenario: Testing how a case where a user adds a product to the cart
-    Given an example scenario
-    When all step definitions are implemented
-    Then the scenario passes
+#   Scenario: Testing how a case where a user adds a product to the cart
+#     Given an example scenario
+#     When all step definitions are implemented
+#     Then the scenario passes
+
+Feature: Student subscribes to a forum discussion, Teacher deletes this discussion
+
+  Scenario: Student subscribes to a forum discussion
+    Given Student is on Home page
+    And Student is logged in with "<username>" and "<password>"
+    And Studnt has a course
+    And Course has a forum discussion
+    When Student subscribes to a forum discussion
+    Then The student succcessfully subscribed to the discussion
+
+    Examples:
+      | username          | password         |
+      | student           | Student123!      |
+
