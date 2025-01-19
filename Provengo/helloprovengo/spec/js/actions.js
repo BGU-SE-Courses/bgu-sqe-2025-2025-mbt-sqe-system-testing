7a084d6a8ab8023aaa1fe67a0f0e38b796380766
Liranitz <liranitz@post.bgu.ac.il>
2025-01-19 11:23:21 +0200

    - remove unnecessary function

--- Provengo/helloprovengo/spec/js/actions.js
+++ Provengo/helloprovengo/spec/js/actions.js
@@ -2,13 +2,6 @@
 //@provengo summon textassertion
 // @provengo summon ctrl
 
-
-SeleniumSession.prototype.runScript = function (script) {
-  bp.log.info("Driver object: " + this.driver);
-  return this.driver.executeScript(script);
-};
-
-
 function adminLogin(session) {
   session.writeText(xpaths.admin.usernameField, 'admin'); // Adjust for your admin credentials
   session.writeText(xpaths.admin.passwordField, '1234'); // Adjust for your admin credentials
@@ -19,14 +12,12 @@ function adminLogin(session) {
 function setProductQuantity(session, data) {
   try {
     bp.log.info("Navigating to Catalog > Products...");
-
     session.waitForClickability(xpaths.admin.catalogMenu, 5000);
     session.click(xpaths.admin.catalogMenu);
     session.waitForClickability(xpaths.admin.productsSubmenu, 5000);
     session.click(xpaths.admin.productsSubmenu); // Navigate to Products
 
     bp.log.info("Navigated to Products page.");
-
     bp.log.info("Opening first product for editing...");
     session.waitForClickability(xpaths.admin.firstProductEdit, 5000);
     session.click(xpaths.admin.firstProductEdit); // Click Edit for the first product
