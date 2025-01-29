ae831f8aab6cbf4be308bcc2029cecd414c30bd1
Ameen Kassem <kassemmo@post.bgu.ac.il>
2025-01-29 18:52:21 +0200

    FinalPushBeforeGoingHome

--- Provengo/moodle/spec/js/actions.js
+++ Provengo/moodle/spec/js/actions.js
@@ -1,19 +1,14 @@
 /* @provengo summon selenium */
 
-function goToLoginScreen(session) {
+function loginAndGoToCourse(session,username,password){
   session.click(xpaths.homePage.loginPageButton)
-}
-
-function enterUserNameAndPassword(session,username,password){
   session.writeText(xpaths.logInPage.usernameBar,username)
   session.writeText(xpaths.logInPage.passwordBar,password)
   session.click(xpaths.logInPage.loginButton)
-}
-
-function goToMyCoursesThenQA_course(session){
   session.click(xpaths.loggedInPage.myCoursesButton)
   session.click(xpaths.loggedInPage.qualityAssuranceButton)
 }
+
 
 function degradeKerenToStudent(session){
   session.click(xpaths.courseHomePage.participantsButton)
@@ -28,43 +23,4 @@ function tryToAddNewContent(session){
   session.click(xpaths.courseHomePage.editModeButton)
   session.click(xpaths.editModePage.addContentButton)
   session.click(xpaths.editModePage.subsectionButton)
-  //session.click(xpaths.editModePage.optionsForSubsectionButton)
-  //session.click(xpaths.editModePage.deleteSubsectionButton)
-  //session.waitFor(xpaths.editModePage.deleteSubsectionButton, 5000);
-  //session.click(xpaths.editModePage.confirmDeletionButton)
-  // addContentButton:"/html/body/div[4]/div[5]/div/div[3]/div/section/div/div/div/ul/li[1]/div[1]/div[2]/div[2]/div/div/button/i",
-  //   subsectionButton:"/html/body/div[4]/div[5]/div/div[3]/div/section/div/div/div/ul/li[1]/div[1]/div[2]/div[2]/div/div/div/a",
-  //   optionsForSubsectionButton:"/html/body/div[4]/div[5]/div/div[3]/div/section/div/div/div/ul/li[1]/div[1]/div[2]/ul/li[2]/div[2]/div/div/ul/li/div/div[1]/div[3]/div/div/div/div/a/i",
-  //   deleteSubsectionButton:"/html/body/div[4]/div[5]/div/div[3]/div/section/div/div/div/ul/li[1]/div[1]/div[2]/ul/li[2]/div[2]/div/div/ul/li/div/div[1]/div[3]/div/div/div/div/div/a[6]/span",
-  //   confirmDeletionButton:
-
 }
-// function clickParticipantsButton(session) {
-//   session.click(xpaths.courseHomePage.participantsButton);
-// }
-
-// function clickEditKerenRoleButton(session) {
-//   session.click(xpaths.courseHomePage.editKerenRoleButton);
-// }
-
-// function clickDeleteTeacherRoleOfKeren(session) {
-//   session.click(xpaths.courseHomePage.deleteTeacherRoleOfKeren);
-// }
-
-// function clickAddStudentRoleOfKeren(session) {
-//   session.click(xpaths.courseHomePage.addStudentRoleOfKeren);
-// }
-
-// function clickConfirmStudentRoleOfKeren(session) {
-//   session.click(xpaths.courseHomePage.confirmStudentRoleOfKeren);
-// }
-
-// function clickSaveKerenRole(session) {
-//   session.click(xpaths.courseHomePage.saveKerenRole);
-// }
-
-
-//   editKerenRoleButton: "",
-//   deleteTeacherRoleOfKeren:"",
-//   addStudentRoleOfKeren:"",
-//   saveKerenRole:""
--- Provengo/moodle/spec/js/behavior.js
+++ Provengo/moodle/spec/js/behavior.js
@@ -1,71 +1,23 @@
 /* @provengo summon selenium */
 
-/**
- * This story opens a new browser window, goes to google.com, and searches for "Pizza".
- */
+// Ahiya downgrades Keren's permissions from Teacher to Student 
+bthread('LoginAndDestroyKerensRights', function () {
+  let s = new SeleniumSession('loginAndDestroyKeren');
+  s.start(URL);
+  loginAndGoToCourse(s,username_1,password_1);
+  degradeKerenToStudent(s);
+})
+
+// Keren tries to add new content to the course
+bthread('LoginAndTryToAddNewContent', function () {
+  let s = new SeleniumSession('loginAndTry');
+  s.start(URL);
+  loginAndGoToCourse(s,username_2,password_2)
+  tryToAddNewContent(s);
+})
+
 
 // function syncWait(ms) {
 //   const start = Date.now();
 //   while (Date.now() - start < ms) {} // Blocking sleep
 // }
-
-// Ahiya downgrades Keren's permissions from Teacher to Student 
-// bthread('LoginAndDestroyKerensRights', function () {
-//   let s = new SeleniumSession('loginAndDestroyKeren');
-//   s.start(URL);
-//   goToLoginScreen(s);
-//   enterUserNameAndPassword(s,username_1,password_1);
-//   goToMyCoursesThenQA_course(s);
-//   degradeKerenToStudent(s);
-
-
-
-//   // goToLoginScreen(s);
-//   // syncWait(1000); // Wait 1 second
-
-//   // enterUserNameAndPassword(s, username_0, password_0);
-//   // syncWait(2000); // Wait 2 seconds
-
-//   // goToMyCoursesThenQA_course(s);
-//   // syncWait(1500); // Wait 1.5 seconds
-
-//   // clickParticipantsButton(s);
-//   // syncWait(1000);
-
-//   // clickEditKerenRoleButton(s);
-//   // syncWait(1000);
-
-//   // clickDeleteTeacherRoleOfKeren(s);
-//   // syncWait(1500);
-
-//   // clickAddStudentRoleOfKeren(s);
-//   // syncWait(1000);
-
-//   // clickConfirmStudentRoleOfKeren(s);
-//   // syncWait(1000);
-
-//   // clickSaveKerenRole(s);
-//   // syncWait(2000); // Final step, wait a bit longer before ending
-// })
-
-// Keren tries to add new content to the course
-
-bthread('LoginAndTryToAddNewContent', function () {
-  let s = new SeleniumSession('loginAndTry');
-  s.start(URL);
-  goToLoginScreen(s);
-  enterUserNameAndPassword(s,username_2,password_2);
-  goToMyCoursesThenQA_course(s);
-  // degradeKerenToStudent(s);
-  tryToAddNewContent(s);
-  
-})
-
-/**
- * This story opens a new browser window, goes to google.com, and searches for "Pasta" using the "I Feel Lucky" feature.
- */
-// bthread('Feeling lucky', function () {
-//   let s = new SeleniumSession('lucky').start(URL)
-//   composeQuery(s, { text: 'Pasta' })
-//   feelLucky(s)
-// })
