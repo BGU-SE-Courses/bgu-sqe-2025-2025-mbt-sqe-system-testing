443d36b9ed397dfb4ceaab035850143d7d3eeab4
amit-levints-bgu <levints@post.bgu.ac.il>
2025-01-30 19:39:03 +0200

    .

--- Cucumber/src/test/java/hellocucumber/QuizAttemptActurator.java
+++ Cucumber/src/test/java/hellocucumber/QuizAttemptActurator.java
@@ -1,6 +1,8 @@
 package hellocucumber;
 
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import java.util.NoSuchElementException;
 
 public class QuizAttemptActurator {
 
@@ -22,7 +25,7 @@ public class QuizAttemptActurator {
         
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(40));
 
-        driver.get("http://localhost/");
+        driver.get("http://localhost:8888/moodle405/");
         driver.manage().window().setPosition(new Point(700, 5));
 
         System.out.println("Init Finished");
@@ -32,14 +35,206 @@ public class QuizAttemptActurator {
         // navigate to log in
         driver.findElement(By.linkText("Log in")).click();
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='username']"))).clear();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='username']"))).sendKeys(Username);
-
-        WebElement webElement = driver.findElement(By.xpath("//*[@id='password']"));
-        webElement.sendKeys(password);
-
+        WebElement usernameField = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='username']")));
+        usernameField.clear();
+        usernameField.sendKeys(Username);
+    
+        WebDriverWait wait2 = new WebDriverWait(driver, Duration.ofSeconds(50));
+        WebElement passwordField = wait2.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='password']")));
+        passwordField.click(); 
+        passwordField.sendKeys(password);
+                    
         driver.findElement(By.id("loginbtn")).click();
+        }
 
+
+    public void navigateToCourse(String courseName){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='frontpage-available-course-list']")));
+
+        // Use JavaScript to scroll down to the 'Available Courses' section
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//div[@id='frontpage-available-course-list']")));
+        
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='courses frontpage-course-list-all']")));
+
+        WebElement courseLink = driver.findElement(By.xpath("//a[contains(@class, 'aalink') and contains(text(), '" + courseName + "')]"));
+        System.out.println("amit2"+courseLink);
+        courseLink.click();
+    }
+
+    public void selectQuiz(String quizName){
+        String xpathExpression = "//a[contains(@href, 'mod/quiz') and .//span[contains(text(), '" + quizName + "')]]";
+        // use the constructed XPath to find the element and click
+        WebElement quizLink = driver.findElement(By.xpath(xpathExpression));
+        quizLink.click();
+    }
+
+    public void startQuiz(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"region-main\"]/div[2]/div[1]/div/div/form"))).click();
+        
+        // Wait for the 'Start attempt' button using multiple attributes
+        WebElement startButton = wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//input[@name='submitbutton']"))
+        );
+        startButton.click();
+    }
+ 
+    public void answerQuiz(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement radioButton = wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//input[@type='radio']")
+        ));
+        
+        // Click on the radio button
+        radioButton.click();
+    }
+
+    public void finishQuiz() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Wait up to 10 seconds
+        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("mod_quiz-next-nav")));  
+        submitButton.click();
+
+        WebElement submitAllButton = wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//button[@type='submit' and contains(@class, 'btn') and contains(@class, 'btn-primary')]")
+        ));
+        submitAllButton.click();
+        WebElement finish = wait.until(ExpectedConditions.elementToBeClickable(
+        By.xpath("//button[@type='button' and contains(@class, 'btn') and contains(@class, 'btn-primary') and @data-action='save']")));
+        finish.click();
     }
     
+    public void markedFinished(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Locate the status cell in the summary table
+        WebElement statusElement = wait.until(ExpectedConditions.visibilityOfElementLocated(
+            By.xpath("//table[contains(@class, 'generaltable')]//tr[th[text()='Status']]/td")
+        ));
+    
+        String statusText = statusElement.getText().trim();
+    
+        // Assertion: Check if the status is "Finished"
+        Assertions.assertEquals("Finished", statusText, "Quiz status is not 'Finished'");    
+
+    }
+
+    public void quizResults(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Locate the grade cell in the summary table
+        WebElement gradeElement = wait.until(ExpectedConditions.visibilityOfElementLocated(
+            By.xpath("//table[contains(@class, 'generaltable')]//tr[th[text()='Grade']]/td")
+        ));
+        
+        String gradeText = gradeElement.getText().trim();
+        
+        // Ensure that the grade text is not empty
+        Assertions.assertFalse(gradeText.isEmpty(), "Grade is missing in the quiz summary");        
+    }
+
+    public void verifyQuizIsAvailable(String quizName) {
+        // Construct the XPath expression to locate the quiz
+        String xpathExpression = "//a[contains(@href, 'mod/quiz') and .//span[contains(text(), '" + quizName + "')]]";
+        
+        try {
+            // Try to find the quiz element using the XPath
+            WebElement quizLink = driver.findElement(By.xpath(xpathExpression));
+            
+            // Assert that the quiz element is displayed (i.e., it is visible on the page)
+            Assertions.assertTrue(quizLink.isDisplayed(), "The quiz '" + quizName + "' is not visible.");
+        } catch (NoSuchElementException e) {
+            // If the quiz is not found, fail the assertion
+            Assertions.fail("The quiz '" + quizName + "' was not found on the page.");
+        }
+    }
+
+    public void hideQuiz(){
+        //change to edit mode
+        WebElement editModeLabel = wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//label[contains(text(), 'Edit mode')]")
+        ));
+        editModeLabel.click();
+        
+        // Wait for the third icon to be visible and click it to open the quiz editing options
+        WebElement threeDotsButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@id='action-menu-toggle-2']")));
+        threeDotsButton.click();
+
+        // Click on the availability option
+        WebElement availabilityOption = wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//span[@class='menu-action-text' and text()='Availability']")
+        ));
+        availabilityOption.click();    
+                
+        // Wait for the "Hide on course page" option to be clickable and click it
+        WebElement hideOption = wait.until(ExpectedConditions.elementToBeClickable(
+            By.linkText("Hide on course page")
+        ));
+        hideOption.click();
+
+    }
+
+    public void verifyQuizIsOnHiddenStatus() {
+        // Try to find the "Hidden from students" message indicating quiz is hidden
+        boolean isHidden = checkIfQuizIsHidden();
+        Assertions.assertTrue(isHidden, "Quiz is not hidden from students.");    
+    }
+    
+    private boolean checkIfQuizIsHidden() {
+        try {
+            // Locate the div with data-region="visibility" and find the span containing "Availability"
+            WebElement visibilityDiv = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("div[data-region='visibility'] span.sr-only")
+            ));
+            
+            // Check if the "Availability" text is present in the span
+            return visibilityDiv.getText().contains("Availability");
+        } catch (Exception e) {
+            return false;
+        }
+    }
+        
+    public void logOut(){
+        driver.findElement(By.className("userbutton")).click();
+        driver.findElement(By.linkText("Log out")).click();
+    }
+
+
+    public void failedGoToQuiz(String quizName) {
+        try {
+            // Attempt to verify if the quiz is available
+            verifyQuizIsAvailable(quizName);
+            // If no exception is thrown, the quiz is accessible (which is incorrect behavior for this method)
+            Assertions.fail("Quiz should be inaccessible but was found");
+        } catch (Exception e) {
+            // Assert that an exception was thrown (indicating the quiz is inaccessible)
+            Assertions.assertTrue(true, "Quiz is inaccessible, as expected");
+        }
+    }
+
+    public void unHideQuiz(String quizName){
+        WebElement thirdIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//i[@class='icon fa fa-ellipsis-v fa-fw '])[4]")));
+        thirdIcon.click();
+
+        // Wait for the hide option to be visible
+        WebElement item = driver.findElement(By.id("actionmenuactionsubpanel-3"));
+        item.click();
+
+        // Click the hide option
+        WebElement element = driver.findElement(By.linkText("Show on course page"));
+        element.click();
+    }
+
+    public void verifyQuizIsClosed() {
+        try {
+            // Try to locate the 'Start Quiz' button (or any other indicator that the quiz is open)
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"region-main\"]/div[2]/div[1]/div/div/form"))).click();            
+            // If the button is clickable, it means the quiz is open
+            Assertions.fail("Quiz is open, 'Start Quiz' button is clickable.");
+        } catch (Exception e) {
+            // If we catch an exception (such as NoSuchElementException or TimeoutException), 
+            // it means the 'Start Quiz' button is unavailable, indicating the quiz is closed.
+            Assertions.assertTrue(true, "Quiz is closed, 'Start Quiz' button is not available.");
+        }
+    }        
+
 }
--- Cucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ Cucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -5,39 +5,176 @@ import io.cucumber.java.en.*;
 import org.junit.jupiter.api.Assertions.*;
 
 public class StepDefinitions {
-    private QuizAttemptActurator acturator;
+    private QuizAttemptActurator first_scenario_acturator;
+    private QuizAttemptActurator second_scenario_acturator;
+    private QuizAttemptActurator third_scenario_acturator;
     private String webDriver = "webdriver.chrome.driver";
-    private String path = "/Users/amitlewinz/WorkSpace/2025-mbt-aq/Selenium/chromedriver.exe";
+    private String path = "/opt/homebrew/bin/chromedriver";
 
+    // Initialize the session for the student
     public void openMoodleInitStudent(){
         System.out.println("Open Moodle at WebPage");
-        acturator = new QuizAttemptActurator();
-        acturator.initSessionAsUser(webDriver, path);
+        first_scenario_acturator = new QuizAttemptActurator();
+        first_scenario_acturator.initSessionAsUser(webDriver, path);
     }
 
-
-    // 
+    // Step to navigate to the Moodle Home Page for the student
     @Given("User is inside the Moodle Home Page")
     public void userIsInsideTheMoodleHomePage() {
         openMoodleInitStudent();
     }
 
-    // 
-    @And("Student is logged in with \"<Username>\" and \"<Password>\"")
+    // Step for student login with credentials
+    @And("Student is logged in with {string} and {string}")
     public void logIn(String Username, String Password) {
-        acturator.logIn(Username, Password);
+        first_scenario_acturator.logIn(Username, Password);
     }
 
-    
-
-    // 
-    @When("all step definitions are implemented")
-    public void allStepDefinitionsAreImplemented() {
+    // Step for student navigating to a specific course
+    @When("Student navigates to the {string}")
+    public void studentNavigaresToTheCourse(String courseName) {
+        first_scenario_acturator.navigateToCourse(courseName);
     }
 
-    // 
-    @Then("the scenario passes")
-    public void theScenarioPasses() {
+    // Step for student selecting a quiz
+    @And("Student selects the quiz {string}")
+    public void studentSelectsTheQuiz(String quizName) {
+        first_scenario_acturator.selectQuiz(quizName);
+    }
+
+    // Step for student clicking to start a quiz attempt
+    @And("Student clicks \"start attemp\"")
+    public void studentClickesStartAttemp() {
+        first_scenario_acturator.startQuiz();
+    }
+
+    // Step for student answering the quiz questions
+    @And("Student answers the quiz questions")
+    public void studentAnswersTheQuizQuestions() {
+        first_scenario_acturator.answerQuiz();
+    }
+
+    // Step for student finishing the quiz attempt
+    @And("Student finish attemp")
+    public void studentFinishAttemp() {
+        first_scenario_acturator.finishQuiz();
+    }
+
+    // Step to check that the quiz attempt is marked as "Finished"
+    @Then("The quiz attempt is marked as \"Finished\"")
+    public void theQuizAttempIsMarkedAsFinished() {
+        first_scenario_acturator.markedFinished();
+    }
+
+    // Step for student to see the quiz results
+    @And("The student sees the quiz results")
+    public void theStudenSeesTheQuizResults() {
+        first_scenario_acturator.quizResults();
+    }
+
+    // Initialize the session for the teacher
+    public void openMoodleInitTeacher(){
+        System.out.println("Open Moodle at WebPage");
+        second_scenario_acturator = new QuizAttemptActurator();
+        second_scenario_acturator.initSessionAsUser(webDriver, path);
+    }
+
+    // Step for teacher to be on the Moodle Home page
+    @Given("Teacher is on Home page")
+    public void teacherIsOnHomePage() {
+        openMoodleInitTeacher();  
+    }
+
+    // Step for teacher login with credentials
+    @And("Teacher is logged in with {string} and {string}")
+    public void teacherIsLoggedIn(String Username, String Password) {
+        second_scenario_acturator.logIn(Username, Password);  
+    }
+
+    // Step for teacher navigating to a specific course page
+    @And("Teacher navigate to course page {string}")
+    public void teacherNavigateToCoursePage(String courseName) {
+        second_scenario_acturator.navigateToCourse(courseName);  
+    }
+
+    // Verify that the quiz is available for the course
+    @And("a quiz titled {string} is available")
+    public void aQuizTitledIsAvailableForTheCourse(String quizName) {
+        second_scenario_acturator.verifyQuizIsAvailable(quizName);  
+    }
+
+    // Step for teacher hiding a specific quiz
+    @And("Teacher hide quiz")
+    public void teacherHideQuiz() {
+        second_scenario_acturator.hideQuiz();  // Teacher hides the specified quiz
+    }
+
+    // Step for verifying the quiz is hidden
+    @And("quiz should be with hidden status")
+    public void quizShouldBeWithHiddenStatus() {
+        second_scenario_acturator.verifyQuizIsOnHiddenStatus();  // Verify that the quiz is hidden
+    }
+
+    // Step for teacher to log out
+    @And("Teacher logged out")
+    public void teacherLoggedOut() {
+        second_scenario_acturator.logOut();  // Log out the current session
+    }
+
+    // Step for student to log in with credentials
+    @When("Student log in with {string} and {string}")
+    public void studentLogIn(String Username, String Password) {
+        openMoodleInitStudent();
+        first_scenario_acturator.logIn(Username, Password);  // Student logs in with provided credentials
+    }
+
+    // Step for student navigating to a specific course page
+    @And("Student navigate to course page {string}")
+    public void studentNavigateToCoursePage(String courseName) {
+        first_scenario_acturator.navigateToCourse(courseName);  // Student navigates to the specified course
+    }
+
+    // Step for student trying to access a quiz and failing
+    @Then("Student try to access quiz and fail {string}")
+    public void studentTryToAccessQuizAndFailed(String quizName) {
+        first_scenario_acturator.failedGoToQuiz(quizName);  
+    }
+
+    // Scenario for "Student tries to access the quiz after the deadline"
+    public void openMoodleInitForDeadlineScenario(){
+        System.out.println("Open Moodle for Deadline Scenario");
+        third_scenario_acturator = new QuizAttemptActurator();
+        third_scenario_acturator.initSessionAsUser(webDriver, path);  // Initialize session for third scenario
+    }
+
+    // Step to initialize the session for the deadline scenario
+    @Given("Anon is inside the Moodle Home Page")
+    public void anonIsInsideTheMoodleHomePageForDeadline() {
+        openMoodleInitForDeadlineScenario();  // Initialize session for the deadline scenario
+    }
+
+    // Step for student to log in with credentials for the deadline scenario
+    @And("Student is log in with {string} and {string}")
+    public void studentIsLoggedInForDeadline(String Username, String Password) {
+        third_scenario_acturator.logIn(Username, Password);  // Student logs in for the deadline scenario
+    }
+
+    // Step for student navigating to a specific course page for the deadline scenario
+    @When("the student navigates to the {string} course page")
+    public void studentNavigatesToCoursePageForDeadline(String courseName) {
+        third_scenario_acturator.navigateToCourse(courseName);  // Student navigates to the course in deadline scenario
+    }
+
+    // Step for student clicking on the quiz in the deadline scenario
+    @And("the student clicks on the quiz titled {string}")
+    public void studentClicksOnTheQuizForDeadline(String quizTitle) {
+        third_scenario_acturator.selectQuiz(quizTitle);  // Student clicks on the quiz in the deadline scenario
+    }
+
+    // Step for student seeing a message indicating the quiz is closed
+    @Then("The student should not see \"start attemp\" button")
+    public void theStudentShouldSeeMessageIndicatingQuizIsClosedForDeadline() {
+        third_scenario_acturator.verifyQuizIsClosed();  // Verify the message for closed quiz
     }
 
 }
